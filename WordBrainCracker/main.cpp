bc40516c5b40518f456f5fec93a66bc7cec6b7f2
mons <msystudio2@gmail.com>
2015-09-25 09:49:24 +0800

    Initial Commit

---
+++ WordBrainCracker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WordBrainCracker
+//
+//  Created by mo on 9/25/15.
+//  Copyright © 2015 mons. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
